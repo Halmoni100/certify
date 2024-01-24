602835da18f509f17b5b768418013002d68d2979
Christopher Hong <chrishong294@gmail.com>
2024-01-24 14:52:35 -0500

    add beast interface

--- include/boost/certify/impl/https_verification.hpp
+++ include/boost/certify/impl/https_verification.hpp
@@ -24,7 +24,17 @@ set_server_hostname(asio::ssl::stream<NextLayer>& stream, string_view hostname)
     std::error_code ec;
     certify::set_server_hostname(stream, hostname, ec);
     if (ec)
-        boost::throw_exception(std::system_error{ec});
+        throw std::system_error{ec};
+}
+
+template<class NextLayer>
+void
+set_server_hostname(beast::ssl_stream<NextLayer>& stream, string_view hostname)
+{
+    std::error_code ec;
+    detail::set_server_hostname(stream.native_handle(), hostname, ec);
+    if (ec)
+        throw std::system_error{ec};
 }
 
 } // namespace certify
